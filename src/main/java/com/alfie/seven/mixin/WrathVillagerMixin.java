5794fe98f8e06ef7ac89d80a8db7de2c1989286c
Alfie <01richardsonalfie@protonmail.com>
2022-05-29 18:21:54 +0100

    wrath gems now gettable

--- src/main/java/com/alfie/seven/mixin/WrathVillagerMixin.java
+++ src/main/java/com/alfie/seven/mixin/WrathVillagerMixin.java
@@ -1,31 +1,31 @@
 package com.alfie.seven.mixin;
 
 import com.alfie.seven.RegisterItems;
-import net.minecraft.block.BellBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BellBlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Block.class)
-public abstract class EnvyBellMixin {
-    @Inject(method = "onBreak",
+@Mixin(VillagerEntity.class)
+public abstract class WrathVillagerMixin {
+    @Inject(method = "onDeath",
             at = @At(value = "HEAD"))
-    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) {
-        if (world.getBlockEntity(pos) instanceof BellBlockEntity) {
+    public void onDeath(DamageSource source, CallbackInfo ci) {
+        Entity entity = source.getAttacker();
+        if (entity != null
+                && entity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) entity;
             PlayerInventory pInv = player.getInventory();
             ItemStack emptyGem = new ItemStack(RegisterItems.EMPTY_GEMSTONE);
             if (pInv.contains(emptyGem)) {
-                ItemStack envyGem = new ItemStack(RegisterItems.ENVY_GEMSTONE);
-                player.giveItemStack(envyGem);
+                ItemStack wrathGem = new ItemStack(RegisterItems.WRATH_GEMSTONE);
+                player.giveItemStack(wrathGem);
                 pInv.removeStack(pInv.getSlotWithStack(emptyGem), 1);
             }
         }
